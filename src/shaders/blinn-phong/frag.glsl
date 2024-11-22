66ed2c6127109a6491ead3f87025956cb97e4658
kunxl-gg <tiwari.25@iitj.ac.in>
2024-11-22 11:06:21 +0530

    fix: remove redundant shaders
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- src/shaders/blinn-phong/frag.glsl
+++ src/shaders/blinn-phong/frag.glsl
@@ -1,6 +1,6 @@
 #version 400 core
 
-struct pointLight {
+struct directionlight {
     vec3 color;
     vec3 direction;
 };
@@ -9,22 +9,22 @@ struct pointLight {
 in vec3 Normals;
 
 // Uniform Variables
-uniform pointLight light;
+uniform directionlight light;
 
 // Output Variables
 out vec4 FragColor;
 
 void main() {
-    vec3 objColor = vec3(1.0, 0.5, 0.2); // Object color as vec3
-    vec3 norm = normalize(Normals);      // Normalize the normal vector
-    vec3 lightDir = normalize(light.direction); // Normalize the light direction
+    vec3 objColor = vec3(1.0, 0.5, 0.2);
+    vec3 norm = normalize(Normals);
+    vec3 lightDir = normalize(light.direction);
 
     // Diffuse lighting calculation
-    float diff = max(dot(norm, lightDir), 0.0); // Clamp to zero if negative
+    float diff = max(dot(norm, lightDir), 0.0);
 
     // Combine object color with light's color and intensity
     vec3 resultColor = objColor * light.color * diff;
 
     // Set the final fragment color
-    FragColor = vec4(resultColor, 1.0); // Alpha set to 1.0 for opaque
+    FragColor = vec4(resultColor, 1.0);
 }
--- src/shaders/mfrag.glsl
+++
@@ -1,8 +0,0 @@
-#version 400 core
-
-// Output Variables
-out vec4 FragColor;
-
-void main() {
-    FragColor = vec4(0.4235f, 0.4588f, 0.4902f, 1.0f);
-}
